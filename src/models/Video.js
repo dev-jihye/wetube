9cb17b09567bdadd61b4e80accc351cd1f273a5c
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-12-01 16:41:24 +0900

    ✨ 정적 메서드 추가

--- src/models/Video.js
+++ src/models/Video.js
@@ -11,5 +11,13 @@ const videoSchema = new mongoose.Schema({
   },
 });
 
+videoSchema.static('formatHashtags', function (hashtags) {
+  return hashtags
+    .split(',')
+    .map((word) =>
+      word.trim().startsWith('#') ? word.trim() : `#${word.trim()}`
+    );
+});
+
 const Video = mongoose.model('Video', videoSchema);
 export default Video;
