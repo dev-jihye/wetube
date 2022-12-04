93c7fdb58d407cf8625e78c09ad33d89faff20f5
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-12-05 01:01:09 +0900

    🚚 라우터 순서 수정

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,18 +3,16 @@ import {
   see,
   logout,
   edit,
-  remove,
   startGithubLogin,
   finishGithubLogin,
 } from '../controllers/userController';
 
 const userRouter = express.Router();
 
-userRouter.get('/:id', see);
 userRouter.get('/logout', logout);
 userRouter.get('/edit', edit);
-userRouter.get('/remove', remove);
 userRouter.get('/github/start', startGithubLogin);
 userRouter.get('/github/finish', finishGithubLogin);
+userRouter.get('/:id', see);
 
 export default userRouter;
