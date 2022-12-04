a1c2960ada6e03aa2cc7331cd525179ad605aafc
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-12-04 22:52:12 +0900

    ✨ 깃허브 로그인 요청 라우터 추가

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,11 @@
 import express from 'express';
-import { see, logout, edit, remove } from '../controllers/userController';
+import {
+  see,
+  logout,
+  edit,
+  remove,
+  startGithubLogin,
+} from '../controllers/userController';
 
 const userRouter = express.Router();
 
@@ -7,5 +13,6 @@ userRouter.get('/:id', see);
 userRouter.get('/logout', logout);
 userRouter.get('/edit', edit);
 userRouter.get('/remove', remove);
+userRouter.get('/github/start', startGithubLogin);
 
 export default userRouter;
