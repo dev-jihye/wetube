d9026f95aff4038d201804e745f6006b3534c4a4
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-12-21 18:07:27 +0900

    change PORT

--- src/init.js
+++ src/init.js
@@ -10,4 +10,4 @@ const PORT = process.env.PORT || 4000;
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🚀`);
 
-app.listen(4000, handleListening);
+app.listen(PORT, handleListening);
