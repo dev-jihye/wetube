9fcd792e8f60dd177177507fc579ef4660b3ac69
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-12-22 14:57:17 +0900

    local환경에서 업로드 시  s3에 업로드하지 않도록 하기

--- src/middlewares.js
+++ src/middlewares.js
@@ -10,16 +10,35 @@ const s3 = new S3Client({
   },
 });
 
-const multerUploader = multerS3({
+const isHeroku = process.env.NODE_ENV === 'production';
+
+const s3ImageUploader = multerS3({
   s3: s3,
   bucket: 'wetube-jihye',
   acl: 'public-read',
+  key: function (request, file, ab_callback) {
+    const newFileName = Date.now() + '-' + file.originalname;
+    const fullPath = 'images/' + newFileName;
+    ab_callback(null, fullPath);
+  },
+});
+
+const s3VideoUploader = multerS3({
+  s3: s3,
+  bucket: 'wetube-jihye',
+  acl: 'public-read',
+  key: function (request, file, ab_callback) {
+    const newFileName = Date.now() + '-' + file.originalname;
+    const fullPath = 'videos/' + newFileName;
+    ab_callback(null, fullPath);
+  },
 });
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = 'Wetube';
   res.locals.loggedInUser = req.session.user || {};
+  res.locals.isHeroku = isHeroku;
   next();
 };
 
@@ -46,7 +65,7 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
-  storage: multerUploader,
+  storage: isHeroku ? s3ImageUploader : undefined,
 });
 
 export const videoUpload = multer({
@@ -54,5 +73,23 @@ export const videoUpload = multer({
   limits: {
     fileSize: 10000000,
   },
-  storage: multerUploader,
+  storage: isHeroku ? s3VideoUploader : undefined,
 });
+
+export const s3DeleteAvatarMiddleware = (req, res, next) => {
+  if (!req.file) {
+    return next();
+  }
+  s3.deleteObject(
+    {
+      bucket: 'wetube-jihye',
+      key: `images/${req.session.user.avatarURL.split('/')[4]}`,
+    },
+    (err, data) => {
+      if (err) {
+        throw err;
+      }
+    }
+  );
+  next();
+};
